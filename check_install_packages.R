f7c32954e0177a4294331193dda6b473a6f9fd18
Mateo W. Racca <52387817+raccamateo@users.noreply.github.com>
2021-06-11 23:59:49 -0300

    Update check_install_packages.R

--- check_install_packages.R
+++ check_install_packages.R
@@ -1,4 +1,4 @@
 install.packages("pacman")
 
-#pacman is going to check if we have the packages and if not it is going to install it
+#pacman is going to check if we have the packages and if not it is going to install it (and also load)
 pacman::p_load(tidyverse, data.table, readODS, tools, lubridate, janitor)
