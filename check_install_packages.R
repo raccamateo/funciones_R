2d8171e05b9ff24609a71cfb8cb6a5fc41452b9f
Mateo W. Racca <52387817+raccamateo@users.noreply.github.com>
2021-06-11 23:59:01 -0300

    Add files via upload

---
+++ check_install_packages.R
@@ -0,0 +1,4 @@
+install.packages("pacman")
+
+#pacman is going to check if we have the packages and if not it is going to install it
+pacman::p_load(tidyverse, data.table, readODS, tools, lubridate, janitor)
